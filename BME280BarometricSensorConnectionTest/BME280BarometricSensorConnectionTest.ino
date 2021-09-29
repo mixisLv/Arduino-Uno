2a3aa060c4d873236076c0c0c03503dbf34051ec
Mikus Rozenbergs <mikus.rozenbergs@gmail.com>
2021-09-29 20:33:30 +0300

    BME280

---
+++ BME280BarometricSensorConnectionTest/BME280BarometricSensorConnectionTest.ino
@@ -0,0 +1,50 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+Adafruit_BME280 bme; // I2C
+
+void setup() {
+    Serial.begin(9600);
+    while(!Serial);    // time to get serial running
+    Serial.println(F("BME280 test"));
+
+    unsigned status;
+    
+    // default settings
+    // (you can also pass in a Wire library object like &Wire2)
+    status = bme.begin();  
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+        Serial.print("        ID of 0x56-0x58 represents a BMP 280,\n");
+        Serial.print("        ID of 0x60 represents a BME 280.\n");
+        Serial.print("        ID of 0x61 represents a BME 680.\n");
+        while (1);
+    }
+    
+    Serial.println("-- Default Test --");
+
+    Serial.println();
+}
+
+
+void loop() { 
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+    delay(5000);
+}
---
+++ BME280BarometricSensorConnectionTest/readme.md
@@ -0,0 +1,7 @@
+## Barometric Temperature Air Pressure Sensor Module (BME280) connection test
+
+* [BME280 pinout](./BME280-pinout.png) 
+* [BME280 connection](./BME280-conncetion.png)
+
+* https://randomnerdtutorials.com/bme280-sensor-arduino-pressure-temperature-humidity/
+
